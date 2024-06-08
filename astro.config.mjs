73abe844bcbc3aec8c88d6e59670c5df53683c9d
Aitor Lozano <info@aitorlozano.com>
2024-06-08 17:29:07 +0100

    workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,8 +5,7 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://aikelb.github.io",
-  base: "cafechinitas",
+  site: "https://cafechinitas.com",
   integrations: [tailwind(), mdx(), sitemap()],
   output: 'static',
 });
